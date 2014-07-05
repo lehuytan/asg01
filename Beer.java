5f8dee06daae1f2aec491659bf8665a1dbcb58cf
duccuchuoi <duccuchuoi1994@gmail.com>
2014-07-04 22:43:41 -0700

    first commit

---
+++ Beer.java
@@ -0,0 +1,11 @@
+
+public class Beer{
+    public static void main ( String [] args){
+	    System.out.println(" \t Ho va ten: Le Huy Tan \n \t Ma sinh vien: 10020305 \n\t Lop: K55CD \n\t username: lehuytan \n\t Mail: tan.lehuy.92@gmail.com \n\n");
+		
+		for(int i=9;i>0;i--){
+		    System.out.println(     i + " bottles of beer on the wall,  bottles of beer \n\t Take one down, pass in around");
+		}
+		System.out.println(" No more bottles of beer on the wall.");
+	}
+}
